f5f55bd7245453faa534f90e69c8b4976a6be12c
inho0987 <inho0987@users.noreply.github.com>
2017-09-19 15:58:08 -0400

    Login page UI

--- app/src/main/java/com/example/cody_/studentchat/MainActivity.java
+++ app/src/main/java/com/example/cody_/studentchat/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.cody_.studentchat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+
+    Button login;
+
+    TextView newAccount;
+    TextView reAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.editTextUsername);
+        password = (EditText)findViewById(R.id.editTextPassword);
+
+        login = (Button)findViewById(R.id.buttonLogin);
+
+        newAccount = (TextView)findViewById(R.id.textViewCreateAccount);
+        reAccount = (TextView)findViewById(R.id.textViewRetrieveAccount);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if username and password is correct
+                //startActivity(new Intent(MainActivity.this, HomeScreen.class));
+
+                //else {
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle(getString(R.string.login_fail));
+                    alertDialog.setMessage(getString(R.string.login_fail_message));
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                //}
+            }
+        });
+
+        newAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateAccount.class));
+            }
+        });
+
+        reAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RetrieveAccount.class));
+            }
+        });
     }
 }
